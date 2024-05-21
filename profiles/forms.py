54617c56d74d6cbd771093e618e7da4be31bb5d0
Ilja Robalkin <robalkin.ilja@gmail.com>
2024-05-21 17:49:38 +0300

    Profile avatar

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,6 +7,16 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ('first_name', 'last_name', 'avatar')
 
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        if self.instance and self.instance.pk:
+            self.fields['avatar'].widget.attrs.update({'onchange': 'previewAvatar();'})
+            self.fields['avatar'].widget.attrs.update({'class': 'form-control'})
+            if self.instance.avatar:
+                self.fields['avatar'].widget.attrs.update({
+                    'data-current-avatar-url': self.instance.avatar.url
+                })
+
 
 class FriendSearchForm(forms.Form):
     email = forms.EmailField(label='Add new friend by email', max_length=100, required=True)
